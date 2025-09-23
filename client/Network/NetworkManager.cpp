ca3c888c0b079e80ef41baa2723e48a003d82b6c
pintat-gil <diego.pintat-gil@gmail.com>
2025-09-23 15:05:06 +0200

    [FIXC] use protocol in client

--- client/Network/NetworkManager.cpp
+++ client/Network/NetworkManager.cpp
@@ -1,44 +1,56 @@
 #include "NetworkManager.hpp"
+#include "../../transferData/opcode.hpp"
+#include "../../transferData/transferData.hpp"
+#include <arpa/inet.h>
+#include <cstring>
 #include <iostream>
 #include <unistd.h>
-#include <cstring>
-#include <arpa/inet.h>
 
-NetworkManager::NetworkManager()
-    : _tcpSocket(-1), _udpSocket(-1) {}
-
-NetworkManager::~NetworkManager() {
-    if (_tcpSocket != -1) close(_tcpSocket);
-    if (_udpSocket != -1) close(_udpSocket);
+NetworkManager::NetworkManager() : _tcpSocket(-1), _udpSocket(-1)
+{
 }
 
-bool NetworkManager::setup(const char* serverIp, int port) {
+NetworkManager::~NetworkManager()
+{
+    if (_tcpSocket != -1)
+        close(_tcpSocket);
+    if (_udpSocket != -1)
+        close(_udpSocket);
+}
+
+bool NetworkManager::setup(const char *serverIp, int port)
+{
     sockaddr_in serverAddr{};
     serverAddr.sin_family = AF_INET;
     serverAddr.sin_port = htons(port);
-    if (inet_pton(AF_INET, serverIp, &serverAddr.sin_addr) <= 0) {
+    if (inet_pton(AF_INET, serverIp, &serverAddr.sin_addr) <= 0)
+    {
         perror("inet_pton");
         return false;
     }
 
     _tcpSocket = socket(AF_INET, SOCK_STREAM, 0);
-    if (_tcpSocket < 0) {
+    if (_tcpSocket < 0)
+    {
         perror("TCP socket");
         return false;
     }
 
-    if (connect(_tcpSocket, (sockaddr*)&serverAddr, sizeof(serverAddr)) < 0) {
+    if (connect(_tcpSocket, (sockaddr *)&serverAddr, sizeof(serverAddr)) < 0)
+    {
         perror("TCP connect");
         return false;
     }
 
     _udpSocket = socket(AF_INET, SOCK_DGRAM, 0);
-    if (_udpSocket < 0) {
+    if (_udpSocket < 0)
+    {
         perror("UDP socket");
         return false;
     }
 
-    if (connect(_udpSocket, (sockaddr*)&serverAddr, sizeof(serverAddr)) < 0) {
+    if (connect(_udpSocket, (sockaddr *)&serverAddr, sizeof(serverAddr)) < 0)
+    {
         perror("UDP connect");
         return false;
     }
@@ -47,55 +59,65 @@ bool NetworkManager::setup(const char* serverIp, int port) {
     _pollFds.push_back({_tcpSocket, POLLIN, 0});
     _pollFds.push_back({_udpSocket, POLLIN, 0});
 
-    std::cout << "NetworkManager setup complete. Connected to " 
-              << serverIp << ":" << port << " (TCP & UDP)" << std::endl;
+    std::cout << "NetworkManager setup complete. Connected to " << serverIp << ":" << port << " (TCP & UDP)"
+              << std::endl;
     return true;
 }
 
-void NetworkManager::loop() {
-    constexpr int TIMEOUT = 1000; // ms
+void NetworkManager::loop()
+{
+    constexpr int TIMEOUT = 1000;
 
-    while (true) {
+    while (true)
+    {
         int ret = poll(_pollFds.data(), _pollFds.size(), TIMEOUT);
-        if (ret < 0) {
+        if (ret < 0)
+        {
             perror("poll");
             break;
         }
-        if (ret == 0) {
+        if (ret == 0)
+        {
             continue;
         }
 
-        for (auto &pfd : _pollFds) {
-            if (pfd.revents & POLLIN) {
-                if (pfd.fd == _tcpSocket) {
-                    char buffer[1024];
-                    ssize_t n = recv(_tcpSocket, buffer, sizeof(buffer), 0);
-                    if (n > 0) {
-                        std::cout << "message tcp: " << buffer << std::endl;
-                        // _receiver.handleTCP(buffer, n);
-                    } else if (n == 0) {
+        for (auto &pfd : _pollFds)
+        {
+            if (pfd.revents & POLLIN)
+            {
+                if (pfd.fd == _tcpSocket)
+                {
+                    std::string buffer;
+                    auto [opcode, paylod] = receiveFrameTCP(_tcpSocket, buffer);
+                    std::cout << "message tcp: " << buffer << std::endl;
+                    // _receiver.handleTCP(buffer, n);
+
+                    if (opcode == OPCODE_CLOSE_CONNECTION)
+                    {
                         std::cerr << "Server closed TCP connection" << std::endl;
                         close(_tcpSocket);
                         _tcpSocket = -1;
                         return;
                     }
-                } else if (pfd.fd == _udpSocket) {
-                    char buffer[1024];
-                    ssize_t n = recv(_udpSocket, buffer, sizeof(buffer), 0);
-                    if (n > 0) {
-                        std::cout << "message udp: " << buffer << std::endl;
-                        // _receiver.handleUDP(buffer, n);
-                    }
+                }
+                else if (pfd.fd == _udpSocket)
+                {
+                    sockaddr_in client{};
+                    socklen_t len = sizeof(client);
+                    auto [opcode, paylod] = receiveFrameUDP(_udpSocket, client, len);
+                    std::cout << "message udp: " << paylod << std::endl;
                 }
             }
         }
     }
 }
 
-Sender& NetworkManager::getSender() {
+Sender &NetworkManager::getSender()
+{
     return _sender;
 }
 
-Receiver& NetworkManager::getReceiver() {
+Receiver &NetworkManager::getReceiver()
+{
     return _receiver;
 }
