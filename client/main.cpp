934b60bcbcbba3434a0beeb87a6c238b509b2a16
LorisDXB <loris.gode@epitech.eu>
2025-09-23 14:12:20 +0200

    [ADD] Correct arguments for client

--- client/main.cpp
+++ client/main.cpp
@@ -7,7 +7,7 @@ int main()
     try {
         ClientGame clientGame;
 
-        clientGame.init();
+        clientGame.init("127.0.0.1", 8081);
         clientGame.start();
     }
     catch(const std::exception& e) {
