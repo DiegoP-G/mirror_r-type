f5d40675f496da491726181e25be69a45f13bf75
LorisDXB <loris.gode@epitech.eu>
2025-09-29 10:45:28 +0200

    [UPD] Pre-commit

--- client/Network/Sender.hpp
+++ client/Network/Sender.hpp
@@ -16,29 +16,8 @@ class Sender
     NetworkECSMediator &_med;
 
   public:
-    // Constructor
     Sender(NetworkECSMediator &med) : _med(med), _tcpSocket(-1), _udpSocket(-1)
     {
-    }
-
-    // Copy constructor
-    Sender(const Sender &other)
-        : _med(other._med), _tcpSocket(other._tcpSocket), _udpSocket(other._udpSocket), _serverAddr(other._serverAddr)
-    {
-        std::cout << "Sender copied" << std::endl;
-    }
-
-    // Copy assignment operator
-    Sender &operator=(const Sender &other)
-    {
-        if (this != &other)
-        {
-            _tcpSocket = other._tcpSocket;
-            _udpSocket = other._udpSocket;
-            _serverAddr = other._serverAddr;
-            // _med cannot be reassigned, still uses original reference
-        }
-        return *this;
     }
 
     void setTcpSocket(int socket)
