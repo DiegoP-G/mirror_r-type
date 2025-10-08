37f7ec18bf86cbf4fc6e298ec7970e4cd85fd514
pintat-gil <diego.pintat-gil@gmail.com>
2025-10-08 13:34:03 +0200

    [ADD] enemyFactory.cpp

--- ecs/enemyFactory.hpp
+++ ecs/enemyFactory.hpp
@@ -7,23 +7,5 @@
 class EnemyFactory
 {
   public:
-    static void createEnemy(EntityManager &entityManager, std::string enemyType, const Vector2D &position)
-    {
-        auto &enemy = entityManager.createEntity();
-        enemy.addComponent<TransformComponent>(position.x, position.y);
-        enemy.addComponent<VelocityComponent>(0.0f, 0.0f);
-        enemy.addComponent<ColliderComponent>(20.0f, 20.0f, true);
-        // 0 for movement type : left
-        // 2 for shooting type : single shot
-        enemy.addComponent<EnemyComponent>(0, 1.0f, 2);
-
-        enemy.addComponent<HealthComponent>(100, 100);
-        enemy.addComponent<HealthBarComponent>(30.0f, 4.0f, -20.0f);
-
-        if (enemyType == "basic_enemy")
-        {
-            enemy.addComponent<AnimatedSpriteComponent>(GraphicsManager::Texture::ENEMY, 98.5, 32.3, 32.8, 32.3, 1,
-                                                        0.05f, -90.0f);
-        }
-    }
+    static void createEnemy(EntityManager &entityManager, std::string enemyType, const Vector2D &position);
 };
