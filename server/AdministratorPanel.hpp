d00838d2979bb2932d6dbf961b33971c9a3c3ed1
EtaLoop <146718995+EtaLoop@users.noreply.github.com>
2025-10-27 13:29:58 +0100

    Restore ban button, fix kick/ban during game/lobby (#58)
    * [FIX] Kicking player in lobby/game
    
    * [RESTORE] Ban button in admin panel

--- server/AdministratorPanel.hpp
+++ server/AdministratorPanel.hpp
@@ -1,6 +1,7 @@
 #pragma once
 #include "../client/assetsPath.hpp"
 #include "Network/ClientManager.hpp"
+#include "Lobby/LobbyManager.hpp"
 #include "NetworkManager.hpp"
 #include "sqlAPI.hpp"
 #include <SFML/Graphics.hpp>
@@ -14,16 +15,19 @@ constexpr int WINDOWWIDTH = 800;
 constexpr int WINDOWHEIGTH = 600;
 
 class ClientManager;
+class LobbyManager;
 
 class AdministratorPanel
 {
   private:
     std::unique_ptr<sf::Font> _font;
     ClientManager *_clientManager;
+    LobbyManager *_lobbyManager;
     NetworkManager &_networkManager;
     sqlAPI _sqlApi;
 
     std::unordered_map<int, sf::RectangleShape> _kickButtons;
+    std::unordered_map<int, sf::RectangleShape> _banButtons;
 
     float _playerListScrollOffset;
     float _logsScrollOffset;
@@ -31,17 +35,19 @@ class AdministratorPanel
     std::vector<std::string> _logs;
 
     void handleEvents(sf::RenderWindow &window);
-    std::vector<std::pair<sf::Text, sf::RectangleShape>> buildPlayerList(sf::RenderWindow &window,
-                                                                         const sf::FloatRect &playerListArea);
+    std::vector<std::tuple<sf::Text, sf::RectangleShape, sf::RectangleShape>> buildPlayerList(
+        sf::RenderWindow &window, const sf::FloatRect &playerListArea);
     void drawPlayerList(sf::RenderWindow &window, sf::FloatRect playerListArea);
     void drawLogs(sf::RenderWindow &window, sf::FloatRect logsArea);
     void scrollLogic(sf::RenderWindow &window, sf::Event &event);
     void kickPlayer(sf::RenderWindow &window, sf::Event &event);
+    void banPlayer(sf::RenderWindow &window, sf::Event &event);
 
   public:
     AdministratorPanel(NetworkManager &networkManager);
 
     void setClientManager(ClientManager &clientManager);
+    void setLobbyManager(LobbyManager &clientManager);
 
     void addLog(std::string log)
     {
